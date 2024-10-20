b73472844584322c4193d8c9240b782c6cc70b15
Shravani3031 <zahidbhaimbbs@gmaIL.COM>
2024-10-20 13:32:47 -0700

    Python

---
+++ day1/lists.py
@@ -0,0 +1,14 @@
+thisList = ["apple", "banana","cherry"]
+
+print(thisList)
+#this is mylist
+
+thisTuple = ("apple", "banana", "cherry")
+
+print(thisTuple)
+#this is myTuple
+
+thisSet = {"apple", "banana", "cherry"}
+
+print(thisSet)
+#This is mySet
