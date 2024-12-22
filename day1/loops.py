03fe34fef291b2d4dbced15c342161ec11b52473
Shravani3031 <zahidbhaimbbs@gmaIL.COM>
2024-12-22 11:33:51 +0530

    feat

---
+++ day1/loops.py
@@ -0,0 +1,11 @@
+for i in range (0,10,2):
+    print (i)
+
+
+  #while loop
+j=0
+while(j<20):
+    j= j+1
+    print (j)
+
+    
---
+++ day1/patterns.py/numeric.py
@@ -0,0 +1,6 @@
+k=0
+for i in range(0,6,2):
+    for j in range(0,6):
+        print(k, end=" ")
+        k=k+1
+    print()
